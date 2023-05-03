ca1e49738696a83af09ede8e2c25e19e8c652ff7
emamjion <emamjion@gmail.com>
2023-05-04 00:26:59 +0600

    footer ui  design finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaMapMarkerAlt, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer here</p>
+        <div className='footer'>
+            <div className='contact-box'>
+                <h3 className='brand-title'>FoodEx.</h3>
+                <h5 className='contact-info'>Connect with Us</h5>
+                <div className='social'>
+                    <Link>< FaFacebook /></Link>
+                    <Link>< FaTwitter /></Link>
+                    <Link>< FaInstagram /></Link>
+                    <Link>< FaLinkedin /></Link>
+                </div>
+            </div>
+            <div className='contact-box'>
+                <h3 className='menu-links'>Our Food</h3>
+                <Link>Beef Items</Link>
+                <Link>Chicken Items</Link>
+                <Link>Vorta Items</Link>
+                <Link>Rice Items</Link>
+            </div>
+            <div className='contact-box address'>
+                <h3>Contact</h3>
+                <span> < FaMapMarkerAlt /> </span>
+                <span> < FaEnvelope /> </span>
+                <span> < FaPhoneAlt /> </span>
+            </div>
         </div>
     );
 };
