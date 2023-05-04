3d57eee3e714844572cab30778770a9546c70976
emamjion <emamjion@gmail.com>
2023-05-04 23:26:19 +0600

    rating icon added

--- src/components/ViewRecipeDetails/ViewRecipeDetails.jsx
+++ src/components/ViewRecipeDetails/ViewRecipeDetails.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './ViewRecipeDetails.css';
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegStar, FaStar } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Rating from "react-rating";
 
 const ViewRecipeDetails = ({ rec }) => {
-  const { recipeImg, recipeName, ingredients, cookingMethod } = rec;
+  const { recipeImg, recipeName, ingredients, cookingMethod, rating } = rec;
   
   const handleFavBtn = () => {
     toast("This Recipe is My Favourite");
@@ -28,6 +29,18 @@ const ViewRecipeDetails = ({ rec }) => {
             <div>
                 <h4 className="">Cooking Method</h4>
                 <p>{cookingMethod}</p>
+            </div>
+            <div className="rating">
+              <span>
+              <Rating
+                placeholderRating={rating}
+                readonly
+                emptySymbol={ < FaRegStar /> }
+              placeholderSymbol={ < FaStar className="text-warning" />  }
+              fullSymbol={ < FaStar /> }
+              />
+              </span>
+              <span className="rating-text">{rating}</span>
             </div>
             <div className="favourite-btn-box">
                 <button onClick={handleFavBtn} className="fav-btn"> <span> < FaHeart /> </span> <span>Favourite</span></button>
